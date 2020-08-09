a9be282274c01fd442eadbb10dc7aa0a022560fa
Kangario <68245183+Kangario@users.noreply.github.com>
2020-08-09 20:28:21 +0300

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,17 @@
+
+
+     
+    let all = document.getElementById ('all');
+    all.onclick = function (){
+        if (document.getElementById ('l')) {document.getElementById ('l').id = 'll';
+        document.getElementById ('l2').id = 'll2';
+        document.getElementById ('l3').id = ('ll3')
+        document.getElementById ('fon').id = 'fon1'}
+        else {document.getElementById ('ll').id = 'l';
+        document.getElementById ('ll2').id = 'l2';
+        document.getElementById ('ll3').id = 'l3';
+        document.getElementById ('fon1').id = 'fon'}
+    }
+
+     
+ 
